ae28e71cb701b96ba705f363971b97b39c895460
Galambur <gaelle.derambure@epsi.fr>
2021-11-16 17:28:42 +0100

    Commencé à faire les vérifications pour la génération des équipes

--- src/concourspetanque/ConcoursPetanque.java
+++ src/concourspetanque/ConcoursPetanque.java
@@ -1,12 +1,28 @@
 package concourspetanque;
 
+import java.lang.Math;
+
 public class ConcoursPetanque {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int max = 36;
+        int min = 12;
+        int range = max - min + 1;
+  
+        // generation du nombre de joueurs 
+        int nombreJoueurs = (int)(Math.random() * range) + min;
+        
+        System.out.println(nombreJoueurs);
+        
+        // generation des equipes
+        double nombreEquipe = 0;
+        nombreEquipe = (double)nombreJoueurs / 2;
+        if(nombreEquipe == 12 || nombreEquipe == 10 || nombreEquipe == 8 || nombreEquipe == 6) {
+            System.out.println("cool");
+        }
     }
     
 }
